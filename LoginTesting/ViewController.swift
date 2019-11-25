5ceef0e5b4ce30e6d3858485c4699b6012d15d07
shahzeb <shahzebyousaf5@gmail.com>
2019-11-26 00:58:32 +0500

    Facebook starter done with logout functionality

--- LoginTesting/ViewController.swift
+++ LoginTesting/ViewController.swift
@@ -7,12 +7,26 @@
 // App ID: 2578875818859612
 
 import UIKit
-
+import FacebookCore
+import FacebookLogin
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let loginButton = FBLoginButton(frame: .zero, permissions: [.publicProfile])
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        
+        if let access = AccessToken.current{
+            print("User is logged in with access \(access)")
+            let vc = storyboard?.instantiateViewController(identifier: "main")
+            navigationController?.pushViewController(vc!, animated: true)
+        }else{
+            print("User is currently logged out.")
+        }
+       
         
     }
     
